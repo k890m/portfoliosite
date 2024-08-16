0ca3fd531b64a299f711d4444991401f93ea772b
Kanmani <94244306+k890m@users.noreply.github.com>
2024-08-15 20:22:50 -0400

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -26,10 +26,11 @@ class AppTestCase(unittest.TestCase):
         print(json)  
         assert "timeline_posts" in json
         assert len(json["timeline_posts"]) == 0
-        '''
+        
 
         response = self.client.post("/api/timeline_post", data={"name": "Alice", "email": "alice@example.com", "content": "This is a test post."})
         assert response.status_code == 200
+        '''
         json = response.get_json()
         print(json)  
         assert "timeline_posts" in json
